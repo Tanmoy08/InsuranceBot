fc175a0a7fa3dce07bfe9915a32623e4970cd457
ramyaprasad <hs_ramya@outlook.com>
2020-08-25 12:01:52 +0530

    UPDATE: Added prompt validations inside insurance dialog

--- dialogs/insurance_renewal_dialog.py
+++ dialogs/insurance_renewal_dialog.py
@@ -4,7 +4,7 @@ from botbuilder.dialogs import (
     WaterfallStepContext,
     DialogTurnResult,
 )
-from botbuilder.dialogs.prompts import TextPrompt, PromptOptions
+from botbuilder.dialogs.prompts import TextPrompt, PromptOptions, NumberPrompt, PromptValidatorContext
 from botbuilder.core import MessageFactory, TurnContext, UserState, CardFactory
 from botbuilder.schema import InputHints, Activity, ActivityTypes, Attachment, ThumbnailCard, CardImage
 from botbuilder.ai.qna import QnAMaker, QnAMakerEndpoint
@@ -15,18 +15,29 @@ from LUISRecognizer import InsuranceQueryRecognizer
 from dataModels import UserData, InsuranceDetails
 from helpers.luis_helper import LuisHelper, Intent
 
+import re
+from datetime import datetime
+from recognizers_number import recognize_number, Culture
+from recognizers_date_time import recognize_datetime
+
+phone = "phone"
+date = "date"
+
 class InsuranceRenewalDialog(ComponentDialog):
     def __init__(self, dialog_id: str = None):
         super(InsuranceRenewalDialog, self).__init__(dialog_id or InsuranceRenewalDialog.__name__)
 
-        self.add_dialog(TextPrompt(TextPrompt.__name__))
+        self.add_dialog(TextPrompt(TextPrompt.__name__, InsuranceRenewalDialog.policynumber_prompt_validator))    
+        self.add_dialog(TextPrompt(phone, InsuranceRenewalDialog.phone_prompt_validator))
+        self.add_dialog(TextPrompt(date, InsuranceRenewalDialog.date_prompt_validator))
+        self.add_dialog(NumberPrompt(NumberPrompt.__name__, InsuranceRenewalDialog.phone_prompt_validator))
         self.add_dialog(
             WaterfallDialog(
                 WaterfallDialog.__name__,
                 [
                     self.check_query_policy_number,
                     self.check_query_mobile_number,
-                    self.check_query_date_of_birth,
+                    self.check_query_date_of_birth,                    
                     self.summarize
                 ],
             )
@@ -38,9 +49,20 @@ class InsuranceRenewalDialog(ComponentDialog):
         if(step_context.options.policy_number != None):
             step_context.values["PolicyNumber"] = step_context.options.policy_number
             return await step_context.next(None)
-        message_text = "Please provide your policy number"
+         
+        else:
+            return await step_context.prompt(
+                    TextPrompt.__name__,
+                    PromptOptions(
+                        prompt=MessageFactory.text("Please provide your policy number."),
+                        retry_prompt=MessageFactory.text(
+                            "Enter a valid policy number."
+                        ),
+                    ),
+                )
+        '''message_text = "Please provide your policy number"
         prompt_message = MessageFactory.text(message_text, message_text, InputHints.expecting_input)
-        return await step_context.prompt(TextPrompt.__name__, PromptOptions(prompt=prompt_message))
+        return await step_context.prompt(TextPrompt.__name__, PromptOptions(prompt=prompt_message))'''
 
     async def check_query_mobile_number(self, step_context: WaterfallStepContext) -> DialogTurnResult:
         if(step_context.result != None):
@@ -48,9 +70,20 @@ class InsuranceRenewalDialog(ComponentDialog):
         if(step_context.options.mobile_number != None):
             step_context.values["MobileNumber"] = step_context.options.mobile_number
             return await step_context.next(None)
-        message_text = "Please provide your mobile number"
+        else:
+            return await step_context.prompt(
+                    phone,
+                    PromptOptions(
+                        prompt=MessageFactory.text("Please provide your phone number."),
+                        retry_prompt=MessageFactory.text(
+                            "Enter a valid phone number."
+                        ),
+                    ),
+                )
+
+        '''message_text = "Please provide your mobile number"
         prompt_message = MessageFactory.text(message_text, message_text, InputHints.expecting_input)
-        return await step_context.prompt(TextPrompt.__name__, PromptOptions(prompt=prompt_message))
+        return await step_context.prompt(TextPrompt.__name__, PromptOptions(prompt=prompt_message))'''
 
     async def check_query_date_of_birth(self, step_context: WaterfallStepContext) -> DialogTurnResult:
         if(step_context.result != None):
@@ -58,9 +91,20 @@ class InsuranceRenewalDialog(ComponentDialog):
         if(step_context.options.date_of_birth != None):
             step_context.values["DateOfBirth"] = step_context.options.date_of_birth
             return await step_context.next(None)
-        message_text = "Please provide your Date of Birth"
+        else:
+            return await step_context.prompt(
+                date,
+                PromptOptions(
+                    prompt=MessageFactory.text("Please provide your date of birth."),
+                    retry_prompt=MessageFactory.text(
+                        "Enter a valid date of birth."
+                    ),
+                ),
+            )
+        '''message_text = "Please provide your Date of Birth"
         prompt_message = MessageFactory.text(message_text, message_text, InputHints.expecting_input)
-        return await step_context.prompt(TextPrompt.__name__, PromptOptions(prompt=prompt_message))
+        return await step_context.prompt(date, PromptOptions(prompt=prompt_message))'''
+
 
     async def summarize(self, step_context: WaterfallStepContext) -> DialogTurnResult:
         if(step_context.result != None):
@@ -81,3 +125,42 @@ class InsuranceRenewalDialog(ComponentDialog):
                 ],
             ) 
         return CardFactory.thumbnail_card(card)
+
+    @staticmethod
+    async def age_prompt_validator(prompt_context: PromptValidatorContext) -> bool:
+        # This condition is our validation rule. You can also change the value at this point.
+        return (
+            prompt_context.recognized.succeeded
+            and 0 < prompt_context.recognized.value < 150
+        )
+
+    @staticmethod
+    async def policynumber_prompt_validator(prompt_context: PromptValidatorContext) -> bool:
+        ret = prompt_context.recognized.succeeded and re.match("[A-Za-z]{2}[0-9]{6}[A-Za-z]", prompt_context.recognized.value)   
+        return ret
+
+    @staticmethod
+    async def phone_prompt_validator(prompt_context: PromptValidatorContext) -> bool:
+        ret = prompt_context.recognized.succeeded and re.match("[0-9]{10}", prompt_context.recognized.value)
+        return ret
+
+    @staticmethod
+    async def date_prompt_validator(prompt_context: PromptValidatorContext) -> bool:
+        is_valid = False
+        results = recognize_datetime(prompt_context.recognized.value, Culture.English)
+        for result in results:
+            for resolution in result.resolution["values"]:
+                if "value" in resolution:
+                    
+                    value = resolution["value"]
+                    if resolution["type"] == "date":
+                        candidate = datetime.strptime(value, "%Y-%m-%d")
+                        is_valid = True
+
+        ret = prompt_context.recognized.succeeded and is_valid
+
+        return ret
+    
+
+
+            
